4a5a94fce07cc2d5419ca386da9da97d4d97105e
Chyroc <chen_yunpeng@foxmail.com>
2017-03-29 22:49:47 +0800

    init

---
+++ app.js
@@ -0,0 +1,45 @@
+//app.js
+const APP_NAME = 'WODEBENZI'
+const SENSE_NAME = `${APP_NAME}_senses`
+
+const getSenses = () =>{
+  return JSON.parse(wx.getStorageSync(SENSE_NAME) || '[]')
+}
+
+const setSense = (sense) => {
+  const senses = getSenses()
+  wx.setStorageSync(SENSE_NAME, JSON.stringify([...senses, sense]))
+}
+
+App({
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+  },
+  getUserInfo:function(cb){
+    var that = this
+    if(this.globalData.userInfo){
+      typeof cb == "function" && cb(this.globalData.userInfo) && console.log(this.globalData.userInfo)
+    }else{
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo) && console.log(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
+  getSenses: getSenses,
+  setSense: setSense,
+  globalData:{
+    userInfo: null,
+    senses: null
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,45 @@
+// index.js
+// 获取应用实例
+var uuidv4 = require('../../lib/uuid/we-uuidv4');
+const _  = require('../../lib/lodash/lodash')
+
+var app = getApp()
+Page({
+  data: {
+    inputValue: '',
+    userInfo: {},
+    senses: []
+  },
+  // 事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  bindFormSubmit: function (e) {
+    if(_.isEmpty(e.detail.value.textarea)){
+      wx.showToast({
+        title: '文字不能为空',
+        icon: 'success',
+        duration: 800
+      })
+    }else{
+      app.setSense(e.detail.value.textarea)
+      this.setData({
+        senses: app.getSenses()
+      })
+    }
+  },
+  onLoad: function () {
+    console.log('onLoad')
+      this.setData({
+      senses: app.getSenses()
+    })
+    var that = this
+    app.getUserInfo(function (userInfo) {
+      that.setData({
+        userInfo: userInfo
+      })
+    })
+  }
+})
