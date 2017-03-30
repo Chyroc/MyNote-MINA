b5d5c8b9f975ae6007d81e9ebb152dd4b73a4715
Chyroc <chen_yunpeng@foxmail.com>
2017-03-30 14:25:20 +0800

    add leancloud

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,6 +2,13 @@
 // 获取应用实例
 var uuidv4 = require('../../lib/uuid/we-uuidv4');
 const _ = require('../../lib/lodash/lodash')
+const AV = require('../../lib/leancloud/av-weapp-min.js')
+
+AV.init({
+  appId: '57K7A5ANQy7pJ0f7KT9dyiU4-gzGzoHsz',
+  appKey: 'XMzvn1x7slgSGNFtnMwBciiW',
+});
+
 
 var app = getApp()
 Page({
@@ -11,29 +18,17 @@ Page({
     senses: [],
     image: ''
   },
-  bindFormSubmit: function (e) {
-    if (!_.isEmpty(e.detail.target.dataset.isamage)) {
-      return
-    }
-    if (_.isEmpty(e.detail.value.textarea)) {
-      wx.showToast({
-        title: '文字不能为空',
-        icon: 'success',
-        duration: 800
+  onLoad: function () {
+    console.log('onLoad')
+    this.setData({
+      senses: app.getSenses()
+    })
+    var that = this
+    app.getUserInfo(function (userInfo) {
+      that.setData({
+        userInfo: userInfo
       })
-    } else {
-      app.setSense({
-        text: e.detail.value.textarea,
-        image: this.data.image
-      })
-      this.setData({
-        senses: app.getSenses()
-      })
-      this.setData({
-        textValue: null,
-        image: ''
-      })
-    }
+    })
   },
   deleteText: function (e) {
     const deletetextindex = e.target.dataset.deletetextindex
@@ -65,16 +60,60 @@ Page({
       urls: [url]
     })
   },
-  onLoad: function () {
-    console.log('onLoad')
-    this.setData({
-      senses: app.getSenses()
-    })
-    var that = this
-    app.getUserInfo(function (userInfo) {
-      that.setData({
-        userInfo: userInfo
+  bindFormSubmit: function (e) {
+    if (!_.isEmpty(e.detail.target.dataset.isamage)) {
+      return
+    }
+    if (_.isEmpty(e.detail.value.textarea)) {
+      wx.showToast({
+        title: '文字不能为空',
+        icon: 'success',
+        duration: 800
       })
-    })
-  }
+    } else {
+      app.setSense({
+        text: e.detail.value.textarea,
+        image: this.data.image
+      })
+      this.setData({
+        senses: app.getSenses()
+      })
+
+      let leancloudImageUrl
+      if (_.isEmpty(this.data.image)) {
+        leancloudImageUrl = ''
+      } else {
+        new AV.File(uuidv4(), {
+          blob: {
+            uri: tempFilePaths[0],
+          },
+        })
+          .save()
+          .then(file => {
+            leancloudImageUrl = file.url()
+          })
+          .catch(console.error);
+      }
+
+      const lcSenses = new AV.Object.extend('Senses')
+      lcSenses.set('text', e.detail.value.textarea)
+      lcSenses.set('image', leancloudImageUrl)
+
+      lcSenses.save().then(function (data) {
+        console.log(data)
+
+      }, function (error) {
+        console.log(error)
+
+      });
+
+
+      this.setData({
+        textValue: null,
+        image: ''
+      })
+
+    }
+  },
+
 })
